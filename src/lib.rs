6b7108708a6c06a33cdd35003c4d30639a37a6c1
Emil Hofstetter <emil.k.hofstetter@gmail.com>
2023-01-11 00:41:13 -0500

    Add list basics

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod base;
+mod tagged;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub(crate) use tagged::MaybeTagged;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+extern crate alloc;
